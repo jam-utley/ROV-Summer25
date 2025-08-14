1120487d497a7e841933e7bdb10ac6d527145bbe
James Utley <james.utley000@gmail.com>
2025-08-14 14:51:37 -0400

    Added second joystick, extended packet

--- Nano_topside_serial_test.ino
+++ Nano_topside_serial_test.ino
@@ -4,9 +4,11 @@
 #define RE 3
 
 SoftwareSerial RS485Serial(10, 11); // TX, RX to Uno
-int joyPin = A0;
-int joyNeutral = 512;
-const int deadzone = 20;
+int joyRight = A0;
+int joyLeft = A1;
+int joyNeutralRight = 512;
+int joyNeutralLeft = 512;
+const int deadzone = 200;
 
 // CRC8 function
 uint8_t crc8(const uint8_t *data, uint8_t len) {
@@ -31,24 +33,37 @@ void setup() {
 
   Serial.begin(2400);      // for debugging
   RS485Serial.begin(2400); // RS485 TX
-  joyNeutral = analogRead(joyPin); // calibrate neutral
+  joyNeutralRight = analogRead(joyRight); // calibrate neutral
+  joyNeutralLeft = analogRead(joyLeft);
 }
 
 void loop() {
-  int joyVal = analogRead(joyPin);
-  char direction = 0;
+  int joyValRight = analogRead(joyRight);
+  int joyValLeft = analogRead(joyLeft);
+  char directionR = 0;
+  char directionL = 0;
+  //assess directionality of right joystick
+  if (joyValRight < joyNeutralRight - deadzone) directionR = 'RF';
+  else if (joyValRight > joyNeutralRight + deadzone) directionR = 'RB';
+  //assess directionality of left joystick
+  if (joyValLeft < joyNeutralLeft - deadzone) directionL = 'LF';
+  else if (joyValLeft > joyNeutralLeft + deadzone) directionL = 'LB';
+  //Serial.println(direction);
 
-  if (joyVal < joyNeutral - deadzone) direction = 'F';
-  else if (joyVal > joyNeutral + deadzone) direction = 'B';
-
-  if (direction != 0) {
-    uint8_t packet[2];
-    packet[0] = direction;
-    packet[1] = crc8(&packet[0], 1);
+  if (directionR != 0 || directionL != 0) {
+    Serial.print("Right: ");
+    Serial.print(joyValRight);
+    Serial.print(" , Left:");
+    Serial.println(joyValLeft);
+    
+    uint8_t packet[3];
+    packet[0] = directionR;
+    packet[1] = directionL;
+    packet[2] = crc8(&packet[0], 1);
 
     digitalWrite(DE, HIGH);
     digitalWrite(RE, HIGH);
-    RS485Serial.write(packet, 2);
+    RS485Serial.write(packet, 3);
     RS485Serial.flush();
     digitalWrite(DE, LOW);
     digitalWrite(RE, LOW);
